c6519ebf20949cbd9423f83f7fe431c0b6b043e8
ManuDev03 <manuprajan03@gmail.com>
2021-05-04 17:14:19 +0530

    Querying single data

--- mongodb.js
+++ mongodb.js
@@ -22,53 +22,13 @@ console.log(`connected correctly`);
 
 const db = client.db(databaseName)
 
-db.collection('users').insertOne({
-    _id:id,
-    name:"Mohanlal",
-    hobbies:"flying jets,riding"
-}, (error, result) => {
-    if(error){
-        return console.log(`unable to insert user`);
-    }
-    console.log(result.ops);
-})
 
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name:'maggie',
-    //         age:25
-    //     },
-    //     {
-    //         name:'maverik',
-    //         age:23
-    //     }
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log(`unable to insert documents`);
-    //     }
-    //     console.log(result.ops);
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: "go for a trip",
-    //         completed: true
-    //     },
-    //     {
-    //         description:"go do a adventure",
-    //         completed: false
-    //     },
-    //     {
-    //         description:'pot plants',
-    //         completed:false
-    //     }
-    // ], (error, result) => {
-    //     if (error){
-    //         return console.log(`unable to insert tasks`);
-    //     }
-    //     console.log(result.ops);
-    // })
-
+   db.collection('users').findOne({name:'maggie'},(error, user) =>{
+       if(error){
+           return console.log(`unable to fetch`);
+       }
+       console.log(user);
+   })
 
 })
