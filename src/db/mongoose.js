bf8d25a33db85ec7f9f726ac77d6ebfd68ed79fc
ManuDev03 <manuprajan03@gmail.com>
2021-05-05 11:35:19 +0530

    creating mongoose model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -14,15 +14,40 @@ const User = mongoose.model('User', {
     }
 })
 
-// model instance
-const me = new User({
-    name: 'Max',
-    age: 27
-})
+// // model instance
+// const me = new User({
+//     name: 'Max',
+//     age: 27
+// })
 
-// save to db
-me.save().then(() => {
-    console.log(me)
-}).catch((error) =>{
-    console.log('Error!', error);
+// // save to db
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) =>{
+//     console.log('Error!', error);
+// })
+
+// 1. Define the model with description and completed fields
+
+const Task = mongoose.model('Task', {
+    descryption: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
 })
+// 2. Create a new instance of the model
+
+const task = new Task({
+    descryption: 'Learn the Mongoose library',
+    completed: false
+})
+// 3. Save the model to the database
+
+task.save().then(() => {
+    console.log(task);
+}).catch((error) => {
+    console.log(error);
+})
+// 4. Test your work
