66d29920f7759a03e13590a6493f674773e930b6
ManuDev03 <manuprajan03@gmail.com>
2021-05-05 17:27:57 +0530

    RestApi Resource creation using Postman

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,7 +12,7 @@ app.post('/users',(req,res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error) => {
 
         res.status(400).send(error)
@@ -20,6 +21,21 @@ app.post('/users',(req,res) => {
     })
 
 })
+
+app.post('/tasks', (req,res) =>{
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+// setup atask creation endpoint
+// 1.create a seperate file for the task model(load it into the index.js)
+// 2.create the task creation endpoint (handle success and error)
+// 3.Test the endpoint from postman with good and bad data
 
 app.listen(port, () => {
     console.log('server is up on port'+ port);
