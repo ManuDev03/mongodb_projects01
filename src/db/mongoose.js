6e2bf3c7bce6f3d3d648b25e856122dc25939264
ManuDev03 <manuprajan03@gmail.com>
2021-05-05 12:45:53 +0530

    email validation in mongoose using validator lib

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
@@ -6,26 +7,46 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 })
 
 const User = mongoose.model('User', {
-    name: {
-        type: String
+    name: 
+    {
+        type: String,
+        required: true
     },
-    age:{
-        type: Number
+    email:
+    {
+        type:String,
+        required: true,
+        validate(value){
+            if(!validator.isEmail(value))
+            {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age:
+    {
+        type: Number,
+        validate(value){
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
-// // model instance
-// const me = new User({
-//     name: 'Max',
-//     age: 27
-// })
+// model instance
+const me = new User({
+    name: 'Mike',
+    email: 'mike@'
 
-// // save to db
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) =>{
-//     console.log('Error!', error);
-// })
+})
+
+// save to db
+me.save().then(() => {
+    console.log(me)
+}).catch((error) =>{
+    console.log('Error!', error);
+})
 
 // 1. Define the model with description and completed fields
 
@@ -37,17 +58,17 @@ const Task = mongoose.model('Task', {
         type: Boolean
     }
 })
-// 2. Create a new instance of the model
+// // 2. Create a new instance of the model
 
-const task = new Task({
-    descryption: 'Learn the Mongoose library',
-    completed: false
-})
-// 3. Save the model to the database
+// const task = new Task({
+//     descryption: 'Learn the Mongoose library',
+//     completed: false
+// })
+// // 3. Save the model to the database
 
-task.save().then(() => {
-    console.log(task);
-}).catch((error) => {
-    console.log(error);
-})
-// 4. Test your work
+// task.save().then(() => {
+//     console.log(task);
+// }).catch((error) => {
+//     console.log(error);
+// })
+// // 4. Test your work
