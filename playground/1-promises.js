6681a9c033b8dfdd1f0f7d912abe87ce02a48dcc
ManuDev03 <manuprajan03@gmail.com>
2021-05-06 11:33:44 +0530

    promise chainig

--- playground/1-promises.js
+++ playground/1-promises.js
@@ -1,22 +1,39 @@
+// // single asynchronus operation
 
-// single asynchronus operation 
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
 
-const add = (a,b) =>{
-    return new Promise((resolve,reject) => {
-        setTimeout(() => {
-            resolve(a+b)
-        },2000)
-    })
-}
+// add(10, 20)
+//   .then((sum) => {
+//     console.log(sum);
 
-add(10,20).then((sum) => {
-    console.log(sum)
+//     add(sum, 5)
+//       .then((sum2) => {
+//         console.log(sum2);
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
 
-    add(sum, 5).then((sum2) => {
-        console.log(sum2);
-    }).catch((e) => {
-        console.log(e);
-    })
-}).catch((e) => {
+// promise chaining
+
+add(1, 1)
+  .then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+  })
+  .then((sum2) => {
+    console.log(sum2);
+  })
+  .catch((e) => {
     console.log(e);
-})
+  });
