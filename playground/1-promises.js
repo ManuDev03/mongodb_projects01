6a4fe57dc3a8d35b40e372c53e51af230d2d4a97
ManuDev03 <manuprajan03@gmail.com>
2021-05-06 11:20:40 +0530

    promises asynchronus operation

---
+++ playground/1-promises.js
@@ -0,0 +1,22 @@
+
+// single asynchronus operation 
+
+const add = (a,b) =>{
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(a+b)
+        },2000)
+    })
+}
+
+add(10,20).then((sum) => {
+    console.log(sum)
+
+    add(sum, 5).then((sum2) => {
+        console.log(sum2);
+    }).catch((e) => {
+        console.log(e);
+    })
+}).catch((e) => {
+    console.log(e);
+})
